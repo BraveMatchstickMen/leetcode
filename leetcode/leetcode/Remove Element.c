604694c52c182d238fb081b096aa823ce5062e89
柴勇峰 <bravetsia@gmail.com>
2016-04-21 00:02:26 +0800

    remove Element

---
+++ leetcode/leetcode/Remove Element.c
@@ -0,0 +1,26 @@
+//
+//  Remove Element.c
+//  leetcode
+//
+//  Created by 柴勇峰 on 4/20/16.
+//  Copyright © 2016 Brave. All rights reserved.
+//
+
+#include "Remove Element.h"
+
+/*
+ Given an array and a value, remove all instances of that value in place and return the new length.
+ 
+ Do not allocate extra space for another array, you must do this in place with constant memory.
+ 
+ The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+ 
+ Example:
+ Given input array nums = [3,2,2,3], val = 3
+ 
+ Your function should return length = 2, with the first two elements of nums being 2.
+ */
+
+int removeElement(int* nums, int numsSize, int val) {
+    
+}
---
+++ leetcode/leetcode/Remove Element.h
@@ -0,0 +1,14 @@
+//
+//  Remove Element.h
+//  leetcode
+//
+//  Created by 柴勇峰 on 4/20/16.
+//  Copyright © 2016 Brave. All rights reserved.
+//
+
+#ifndef Remove_Element_h
+#define Remove_Element_h
+
+#include <stdio.h>
+
+#endif /* Remove_Element_h */
